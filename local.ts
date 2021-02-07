884e9ff6c8d87e37be3eca346d131273181ea763
Pritesh <pritesh@Priteshs-MacBook-Air.local>
2021-02-07 23:59:14 +0530

    Husky Testing

--- local.ts
+++ local.ts
@@ -6,5 +6,5 @@
 import server from "./src";
 import logger from "./src/logger";
 server.listen(3000, () => {
-    logger.info(`instance running on port ${3000}`)
+logger.info(`instance running on port ${3000}`)
 });
