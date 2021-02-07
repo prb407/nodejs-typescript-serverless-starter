5fb9901dd027eb6940dadb54712714cd3436940e
Pritesh <pritesh@voxxup.com>
2021-02-08 00:14:55 +0530

    fix(develop): testing Husky

--- local.ts
+++ local.ts
@@ -2,9 +2,8 @@
  * @author Pritesh Bhanderi
  */
 
-
-import server from "./src";
-import logger from "./src/logger";
+import server from './src';
+import logger from './src/logger';
 server.listen(3000, () => {
-logger.info(`instance running on port ${3000}`)
+	logger.info(`instance running on port ${3000}`);
 });
